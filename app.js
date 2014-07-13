f86f204d5995faaa5aede30bd88eee6115d8dbcf
Vaibhav kohli <vaibhav.khl@gmail.com>
2014-07-13 14:20:37 +0530

    adding watch on count

--- app.js
+++ app.js
@@ -11,17 +11,31 @@ barApp.directive('databar', function() {
       elementTwoCount: '=elementTwoCount'
     },
     link: function(scope, element) {
-    scope.elementOneWidth = scope.elementOneCount/scope.lastElementCount * 100;
-    scope.elementTwoWidth = scope.elementTwoCount/scope.lastElementCount * 100;
-    console.log(scope.elementOneWidth);
-    console.log(scope.elementTwoWidth);
+    var calculateWidth = function() {
+		  scope.elementOneWidth = scope.elementOneCount/scope.lastElementCount * 100;
+		  scope.elementTwoWidth = scope.elementTwoCount/scope.lastElementCount * 100;
+		  console.log(scope.elementOneWidth);
+		  console.log(scope.elementTwoWidth);	
+    };
+    scope.$watchCollection('[elementOneCount, elementTwoCount, lastElementCount]', calculateWidth);
     },
     templateUrl: 'data-bar.html'
   };
 });
 
 barApp.controller('mainCtrl', function($scope) {
-  $scope.elementOneCount = 150;
-  $scope.elementTwoCount = 180;
+  $scope.elementOneCount = 100;
+  $scope.elementTwoCount = 200;
   $scope.lastElementCount = 300;
+  
+  $scope.changeData = function() {
+	$scope.elementOneCount = 50;
+	$scope.elementTwoCount = 150;
+  };
+
+  $scope.changeLastCount = function() {
+  	$scope.lastElementCount = 200;
+  }
 });
+
+
